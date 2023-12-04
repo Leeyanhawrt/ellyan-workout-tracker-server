b209604607f3e1937ec5ac3e7a5681de3553ef08
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-04 14:36:43 -0500

    Add users route for adding bodyweight and gender

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+const authorization = require("../middleware/authorization");
 module.exports = (pool) => {
     /* GET all users */
     router.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -24,6 +25,17 @@ module.exports = (pool) => {
         catch (err) {
             console.log(err);
             res.status(500).json("Server Error");
+        }
+    }));
+    router.post("/", authorization, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            const { gender, weight } = req.body;
+            const data = yield pool.query(`UPDATE users SET gender = $1, bodyweight = $2 WHERE id = $3;`, [gender, weight, req.user]);
+            res.status(200).send(data.rows);
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).json("Server Error Adding Profile Information");
         }
     }));
     return router;
--- routes/users.ts
+++ routes/users.ts
@@ -1,5 +1,6 @@
 import express from "express";
 const router = express.Router();
+const authorization = require("../middleware/authorization");
 import { Request, Response, NextFunction } from "express";
 import { Pool } from "pg";
 
@@ -12,6 +13,20 @@ module.exports = (pool: Pool) => {
     } catch (err) {
       console.log(err);
       res.status(500).json("Server Error");
+    }
+  });
+
+  router.post("/", authorization, async (req: Request, res: Response) => {
+    try {
+      const { gender, weight } = req.body;
+      const data = await pool.query(
+        `UPDATE users SET gender = $1, bodyweight = $2 WHERE id = $3;`,
+        [gender, weight, req.user]
+      );
+      res.status(200).send(data.rows);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json("Server Error Adding Profile Information");
     }
   });
 
