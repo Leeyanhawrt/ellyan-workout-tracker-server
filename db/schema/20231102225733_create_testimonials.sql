ffa73bef37846b517c94c450a23b2143e7c39ac0
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-04 23:23:15 -0500

    Change lorem text to legit testimonial speech

--- db/schema/20231102225733_create_testimonials.sql
+++ db/schema/20231102225733_create_testimonials.sql
@@ -6,6 +6,6 @@ CREATE TABLE testimonials(
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
-  review_description VARCHAR(255) NOT NULL,
+  review_description TEXT NOT NULL,
   profile_image_path VARCHAR(255) NOT NULL
 );
