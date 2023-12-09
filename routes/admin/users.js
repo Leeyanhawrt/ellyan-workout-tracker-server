ef5cec5649e7f2d3fedc00d6b494172d7a2141fa
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-08 19:31:51 -0500

    Remove unneccessary fields from admin users

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -24,8 +24,6 @@ module.exports = (pool) => {
           email,
           users.id,
           gender, 
-          bodyweight, 
-          workout_program_id AS "workoutProgramId", 
           workout_programs.name AS "workoutProgramName" 
         FROM users
         JOIN workout_programs ON users.workout_program_id = workout_programs.id;`);
--- routes/admin/users.ts
+++ routes/admin/users.ts
@@ -15,8 +15,6 @@ module.exports = (pool: Pool) => {
           email,
           users.id,
           gender, 
-          bodyweight, 
-          workout_program_id AS "workoutProgramId", 
           workout_programs.name AS "workoutProgramName" 
         FROM users
         JOIN workout_programs ON users.workout_program_id = workout_programs.id;`
