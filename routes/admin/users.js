1e35ba117f6db99d68ce7d5e831526246f387931
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-10 11:01:30 -0800

    Send all user data to admin users route to display

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -37,7 +37,7 @@ module.exports = (pool) => {
     }));
     router.get("/:id", authorization, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         try {
-            const user = yield pool.query(`SELECT first_name AS "firstName", last_name AS "lastName" FROM users WHERE id = $1`, [req.params.id]);
+            const user = yield pool.query('SELECT first_name AS "firstName", last_name AS "lastName", email, gender, bodyweight, id, workout_program_id AS "workoutProgramId" FROM users WHERE id = $1', [req.params.id]);
             res.json(user.rows[0]);
         }
         catch (err) {
