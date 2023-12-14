fd3e264574e9b3516dfdc15a9af67c09fd59e7ec
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-13 20:32:18 -0800

    Try changing casing

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ const authenticationRoutes = require("./routes/jwtAuth");
 const profileRoutes = require("./routes/users");
 const testimonialRoutes = require("./routes/testimonials");
 const dashboardRoutes = require("./routes/dashboard");
-const workoutProgramRoutes = require("./routes/workoutProgram");
+const workoutProgramRoutes = require("./routes/workoutPrograms");
 
 // Admin Resource Routes
 const adminUserRoutes = require("./routes/admin/users");
