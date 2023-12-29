44a0f259e74c816b4c49a32b7e1a63bca2fcb535
Leeyanhawrt <leeyan1108@gmail.com>
2023-12-29 16:21:01 -0500

    Make changes to feature branch front end

--- routes/workoutPrograms.ts
+++ routes/workoutPrograms.ts
@@ -90,6 +90,8 @@ module.exports = (pool: Pool) => {
           [req.params.id]
         );
 
+        const frontEnd = "in progress";
+
         res.json(exercises.rows);
       } catch (err) {
         console.log(err);
