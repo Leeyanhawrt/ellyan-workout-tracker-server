52eb7215e097cfce705f613eb1c1dc2206aa75fc
Leeyanhawrt <leeyan1108@gmail.com>
2024-01-11 14:14:18 -0500

    Work on seperate branch

--- routes/workoutPrograms.ts
+++ routes/workoutPrograms.ts
@@ -90,6 +90,8 @@ module.exports = (pool: Pool) => {
           [req.params.id]
         );
 
+        console.log("Jan 11");
+
         res.json(exercises.rows);
       } catch (err) {
         console.log(err);
