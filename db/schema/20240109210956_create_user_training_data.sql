7af939b1011214426aa1039e9ce26438085a9fd7
Leeyanhawrt <leeyan1108@gmail.com>
2024-01-12 19:39:46 -0500

    Change decimal to array of decimals

--- db/schema/20240109210956_create_user_training_data.sql
+++ db/schema/20240109210956_create_user_training_data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user_workouts(
   id SERIAL PRIMARY KEY,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  rpe DECIMAL,
+  rpe DECIMAL[],
   user_id INT REFERENCES users(id) ON DELETE CASCADE,
   workout_exercise_id INT REFERENCES workout_exercises(id) ON DELETE CASCADE
 );
